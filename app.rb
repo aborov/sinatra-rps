ee4d44deb84cf22717445100917fe4c267497f4a
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-03-16 14:39:31 +0000

    started on moves

--- app.rb
+++ app.rb
@@ -8,11 +8,15 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-get("/") do
+get('/') do
   erb(:rules)
 end
 
 get('/rock') do
+  move = rand(1..3)
+  if move == 1
+    
+  end
   erb(:rock)
 end
 
@@ -20,6 +24,6 @@ get('/paper') do
   erb(:paper)
 end
 
-get('scissors') do
+get('/scissors') do
   erb(:scissors)
 end
