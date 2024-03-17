74d8fdaba3b4df47c9302a360e03df7da6da1111
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-03-17 17:06:20 +0000

    final fixes

--- app.rb
+++ app.rb
@@ -8,22 +8,44 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+def determine_winner(our_move, computer_move)
+  if our_move == computer_move
+    return 'We tied!', computer_move
+  elsif (our_move == 1 && computer_move == 3) || (our_move == 2 && computer_move == 1) || (our_move == 3 && computer_move == 2)
+    return 'We won!', computer_move
+  else
+    return 'We lost!', computer_move
+  end
+end
+
+def generate_move
+  rand(1..3)
+end
+
 get('/') do
   erb(:rules)
 end
 
 get('/rock') do
-  move = rand(1..3)
-  if move == 1
-    
-  end
+  our_move = 1
+  computer_move = generate_move
+
+  @outcome, @computer_move = determine_winner(our_move, computer_move)
   erb(:rock)
 end
 
 get('/paper') do
+  our_move = 2
+  computer_move = generate_move
+
+  @outcome, @computer_move = determine_winner(our_move, computer_move)
   erb(:paper)
 end
 
 get('/scissors') do
+  our_move = 3
+  computer_move = generate_move
+
+  @outcome, @computer_move = determine_winner(our_move, computer_move)
   erb(:scissors)
 end
